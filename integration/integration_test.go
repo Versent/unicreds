cb10851db7a70352e049e200e2ff411b725742bc
Mark Wolfe <mark@wolfe.id.au>
2016-07-07 20:24:04 +1000

    Fixing neglected integration test.

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -1,8 +1,7 @@
-// +build integration
-
 package integration
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/apex/log"
@@ -19,21 +18,29 @@ func init() {
 
 func TestIntegrationGetSecret(t *testing.T) {
 
+	var err error
+
 	unicreds.SetRegion(aws.String("us-west-2"))
 
-	err := unicreds.PutSecret("alias/accounting", "Integration1", "secret1", "")
-	assert.Nil(t, err)
+	for i := 0; i < 15; i++ {
+		err = unicreds.PutSecret(aws.String("credential-store"), "alias/accounting", "Integration1", "secret1", fmt.Sprintf("%d", i))
+		if err != nil {
+			log.Errorf("put err: %v", err)
+		}
 
-	cred, err := unicreds.GetSecret("Integration1")
+		assert.Nil(t, err)
+	}
+
+	cred, err := unicreds.GetSecret(aws.String("credential-store"), "Integration1")
 	assert.Nil(t, err)
 	assert.Equal(t, cred.Name, "Integration1")
 	assert.Equal(t, cred.Secret, "secret1")
 
-	creds, err := unicreds.GetAllSecrets(true)
+	creds, err := unicreds.GetAllSecrets(aws.String("credential-store"), true)
 	assert.Nil(t, err)
-	assert.Len(t, creds, 1)
+	assert.Len(t, creds, 24)
 
-	err = unicreds.DeleteSecret("Integration1")
+	err = unicreds.DeleteSecret(aws.String("credential-store"), "Integration1")
 	assert.Nil(t, err)
 
 }
