1fb92b613c343a25c56916feeae62232cc4599a4
Josh Wong <josh.wong@versent.com.au>
2017-01-03 11:24:46 +1100

    Update manifest and install script based on feedback from package review process

--- packaging/chocolatey/tools/chocolateyinstall.ps1
+++ packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -1,13 +1,14 @@
 $ErrorActionPreference = 'Stop';
 
-$packageName= 'unicreds' # arbitrary name for the package, used in messages
+$packageName= 'unicreds'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = '{{DownloadUrl}}' # download url for 32-bit
-$url64      = 'https://github.com/Versent/unicreds/releases/download/1.5.1/unicreds_1.5.1_windows_amd64.tar.gz' #
+$url64      = 'https://github.com/Versent/unicreds/releases/download/1.5.1/unicreds_1.5.1_windows_amd64.tar.gz'
 
 Install-ChocolateyZipPackage -PackageName $packageName `
   -Url64 $url64 `
-  -UnzipLocation $toolsDir
+  -UnzipLocation $toolsDir `
+  -ChecksumType 'md5' `
+  -Checksum64 '75D8B2496675A2AFDC41F865472AB18A'
 
 $filename = [System.IO.Path]::GetFileNameWithoutExtension($(Split-Path $url64 -Leaf))
 
