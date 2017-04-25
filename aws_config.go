e23f40d989cfa73d0f30ad23b1252109d5813e5f
Steve Hoffman <shoffman@journera.com>
2017-04-25 14:26:16 -0500

    Add AssumeRole support
    Based on the sample AssumeRole code here:
    https://docs.aws.amazon.com/sdk-for-go/api/aws/credentials/stscreds/
    
    I've added an additional optional flag for a Role ARN.
    If specified, I used the configuration (with our without a profile)
    to request temporary credentials via the STS service.
    
    These credentials are then passed to the DynamoDB and Kinesis modules.
    
    Not really sure why a single session isn't created at the start
    and passed to the other modules, but I'll leave that optimization
    for another developer to fix

--- aws_config.go
+++ aws_config.go
@@ -6,6 +6,8 @@ import (
 	"github.com/apex/log"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 const (
@@ -14,7 +16,7 @@ const (
 
 // SetAwsConfig configure the AWS region with a fallback for discovery
 // on EC2 hosts.
-func SetAwsConfig(region, profile *string) (err error) {
+func SetAwsConfig(region, profile *string, role *string) (err error) {
 	if region == nil {
 		// Try to get our region based on instance metadata
 		region, err = getRegion()
@@ -33,18 +35,28 @@ func SetAwsConfig(region, profile *string) (err error) {
 		return fmt.Errorf("Must provide a region flag when specifying a profile")
 	}
 
-	setAwsConfig(region, profile)
+	setAwsConfig(region, profile, role)
 	return nil
 }
 
-func setAwsConfig(region, profile *string) {
+func setAwsConfig(region, profile *string, role *string) {
 	log.WithFields(log.Fields{"region": aws.StringValue(region), "profile": aws.StringValue(profile)}).Debug("Configure AWS")
 	config := &aws.Config{Region: region}
+
 	// if a profile is supplied then just use the shared credentials provider
 	// as per docs this will look in $HOME/.aws/credentials if the filename is ""
 	if aws.StringValue(profile) != "" {
 		config.Credentials = credentials.NewSharedCredentials("", *profile)
 	}
+
+	// Are we assuming a role?
+	if aws.StringValue(role) != "" {
+		// Must request credentials from STS service and replace before passing on
+		sess := session.Must(session.NewSession(config))
+		log.WithFields(log.Fields{"role": aws.StringValue(role)}).Debug("AssumeRole")
+		config.Credentials = stscreds.NewCredentials(sess, *role)
+	}
+
 	SetDynamoDBConfig(config)
 	SetKMSConfig(config)
 }
