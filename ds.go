8155bcb268e66c4ac2f87faf5bb186e587d112fc
Joshua Morris <josh@masteryconnect.com>
2017-06-22 15:09:15 +0000

    Merged in version 1.5.1 from upstream

--- ds.go
+++ ds.go
@@ -158,28 +158,17 @@ func Setup(tableName *string, read *int64, write *int64) (err error) {
 func GetHighestVersionSecret(tableName *string, name string, encContext *EncryptionContextValue) (*DecryptedCredential, error) {
 	log.Debug("Getting highest version secret")
 
-	exprAttrVal := map[string]*dynamodb.AttributeValue{
-		":name": &dynamodb.AttributeValue{
-			S: aws.String(name),
-		},
-	}
-	var keyCondExpr *string
-	if len(version) > 0 {
-		exprAttrVal[":version"] = &dynamodb.AttributeValue{
-			S: aws.String(version),
-		}
-		keyCondExpr = aws.String("#N = :name AND version = :version")
-	} else {
-		keyCondExpr = aws.String("#N = :name")
-	}
-
 	res, err := dynamoSvc.Query(&dynamodb.QueryInput{
 		TableName: tableName,
 		ExpressionAttributeNames: map[string]*string{
 			"#N": aws.String("name"),
 		},
-		ExpressionAttributeValues: exprAttrVal,
-		KeyConditionExpression:    keyCondExpr,
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":name": &dynamodb.AttributeValue{
+				S: aws.String(name),
+			},
+		},
+		KeyConditionExpression: aws.String("#N = :name"),
 		Limit:            aws.Int64(1),
 		ConsistentRead:   aws.Bool(true),
 		ScanIndexForward: aws.Bool(false), // descending order
