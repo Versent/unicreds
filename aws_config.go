5bd67bb3b7f9b575f8ff24022f5c01d9a382460c
Don Kuntz <dkuntz@journera.com>
2018-04-30 16:33:08 -0500

    Fix AWS session configuration for differences between using a profile and using a role + profile
    Before, using `unicreds --profile foo` would error out, while
    `AWS_PROFILE=foo unicreds` would not. This was a mistake, and the
    correct action is they should both work correctly.
    
    This commit will make the --profile flag act the same as using the
    AWS_PROFILE / AWS_DEFAULT_PROFILE environment variables, and also ensure
    unicreds continues to work when using role assumption.

--- aws_config.go
+++ aws_config.go
@@ -37,27 +37,35 @@ func SetAwsConfig(region, profile *string, role *string) (err error) {
 
 func setAwsConfig(region, profile *string, role *string) {
 	log.WithFields(log.Fields{"region": aws.StringValue(region), "profile": aws.StringValue(profile)}).Debug("Configure AWS")
-	config := aws.Config{Region: region}
 
-	// if a profile is supplied then just use the shared credentials provider
-	// as per docs this will look in $HOME/.aws/credentials if the filename is ""
-	if aws.StringValue(profile) != "" {
-		config.Credentials = credentials.NewSharedCredentials("", *profile)
-	}
-
-	// Are we assuming a role?
-	if aws.StringValue(role) != "" {
-		// Must request credentials from STS service and replace before passing on
-		sts_sess := session.Must(session.NewSession(&config))
-		log.WithFields(log.Fields{"role": aws.StringValue(role)}).Debug("AssumeRole")
-		config.Credentials = stscreds.NewCredentials(sts_sess, *role)
-	}
-
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		Config:            config,
-		SharedConfigState: session.SharedConfigEnable,
-	}))
+	sess := getAwsSession(region, profile, role)
 
 	SetDynamoDBSession(sess)
 	SetKMSSession(sess)
+}
+
+func getAwsSession(region, profile, role *string) *session.Session {
+	config := aws.Config{Region: region}
+
+	// If a role is supplied, get credentials from STS Session
+	if aws.StringValue(role) != "" {
+		// If the role is being assumed through a non-default profile it must be added to the config.
+		// When an empty string is provided, it uses the default credentials file.
+		if aws.StringValue(profile) != "" {
+			config.Credentials = credentials.NewSharedCredentials("", *profile)
+		}
+
+		sts_session := session.Must(session.NewSession(&config))
+		log.WithFields(log.Fields{"role": aws.StringValue(role), "profile": aws.StringValue(profile)}).Debug("AssumeRole")
+		config.Credentials = stscreds.NewCredentials(sts_session, *role)
+
+		return session.Must(session.NewSession(&config))
+	}
+
+	// If no role is supplied, use the shared AWS config
+	return session.Must(session.NewSessionWithOptions(session.Options{
+		Config:            config,
+		SharedConfigState: session.SharedConfigEnable,
+		Profile:           *profile,
+	}))
 }
