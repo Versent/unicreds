49e8918e3a6843089dcccb6132268e5b9a373872
Zbyněk Vymazal <zbynek@vivino.com>
2021-01-21 15:05:59 +0100

    Re-generate mocks

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/apex/log/handlers/cli"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/stretchr/testify/assert"
-	"github.com/versent/unicreds"
+	"github.com/Vivino/unicreds"
 )
 
 func init() {
