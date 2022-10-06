68cc9dbeda617a5b5b3f9a78d421977cea3ea2b4
Leandro Forain <leandro.forain@instructure.com>
2022-10-06 15:50:10 +0000

    MCE-16908 Optional endpoint override argument

--- aws_config_test.go
+++ aws_config_test.go
@@ -9,18 +9,18 @@ import (
 
 func TestConfig(t *testing.T) {
 
-	err := SetAwsConfig(nil, nil, nil)
+	err := SetAwsConfig(nil, nil, nil, nil)
 	assert.Nil(t, err)
 
-	err = SetAwsConfig(aws.String(""), aws.String(""), aws.String(""))
+	err = SetAwsConfig(aws.String(""), aws.String(""), aws.String(""), aws.String(""))
 	assert.Nil(t, err)
 
-	err = SetAwsConfig(aws.String(""), aws.String("wolfeidau"), aws.String(""))
+	err = SetAwsConfig(aws.String(""), aws.String("wolfeidau"), aws.String(""), aws.String(""))
 	assert.Error(t, err)
 
-	err = SetAwsConfig(aws.String("us-west-2"), aws.String("wolfeidau"), aws.String(""))
+	err = SetAwsConfig(aws.String("us-west-2"), aws.String("wolfeidau"), aws.String(""), aws.String(""))
 	assert.Nil(t, err)
 
-	err = SetAwsConfig(aws.String("us-west-2"), aws.String("wolfeidau"), aws.String("role"))
+	err = SetAwsConfig(aws.String("us-west-2"), aws.String("wolfeidau"), aws.String("role"), aws.String("localstack"))
 	assert.Nil(t, err)
 }
